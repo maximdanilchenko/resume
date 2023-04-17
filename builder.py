8a3d080dec18b9063be7455fb0c5ff4935401185
Maksim Danilchenko <maksimdanilchenko@indrives-MBP.fritz.box>
2023-04-17 14:41:56 +0300

    add index.html

--- builder.py
+++ builder.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     with open("resume.tmp.html") as blog_tmp:
         tmp = Template(blog_tmp.read())
 
-    with open("readme.md") as index_md, open("build/resume.html", "w") as index_html:
+    with open("readme.md") as index_md, open("build/index.html", "w") as index_html:
         index_html.write(
             tmp.render(
                 body=mistune.markdown(index_md.read(), escape=False)
